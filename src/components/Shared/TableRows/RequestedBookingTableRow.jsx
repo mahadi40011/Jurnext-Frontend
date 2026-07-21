cf9253abfca28b52b237c6a37f354b360b3fb7b6
mahadi40011 <mahadi40011@gmail.com>
2026-07-21 14:33:54 +0600

    update $ to TK

--- src/components/Shared/TableRows/RequestedBookingTableRow.jsx
+++ src/components/Shared/TableRows/RequestedBookingTableRow.jsx
@@ -48,7 +48,7 @@ const RequestedBookingTableRow = ({refetch, bookingReqData }) => {
         </p>
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm">
-        <p className="text-gray-900 ">${ticketPrice * quantity}</p>
+        <p className="text-gray-900 ">{ticketPrice * quantity} TK</p>
       </td>
 
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
