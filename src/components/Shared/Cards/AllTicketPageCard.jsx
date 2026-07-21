a90704de6b2edb2a86a3171066403ede70dea3fe
mahadi40011 <mahadi40011@gmail.com>
2026-07-21 09:35:25 +0600

    changed the ticket card of all ticket page

--- src/components/Shared/Cards/AllTicketPageCard.jsx
+++ src/components/Shared/Cards/AllTicketPageCard.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router";
-import {
-
-  FaClock,
-  FaCalendarAlt,
-  FaChevronRight,
-} from "react-icons/fa";
+import { FaClock, FaCalendarAlt, FaChevronRight } from "react-icons/fa";
 
 const AllTicketPageCard = ({ ticket }) => {
-  const { _id, image, operator, perks, price, quantity, time, date, from, to } =
-    ticket;
+  const {
+    _id,
+    image,
+    operator,
+    busType,
+    price,
+    quantity,
+    time,
+    date,
+    from,
+    to,
+  } = ticket;
 
   return (
     <div className="bg-white rounded-3xl shadow-sm hover:shadow-2xl transition-all duration-500 border border-gray-100 overflow-hidden flex flex-col group">
@@ -21,8 +26,17 @@ const AllTicketPageCard = ({ ticket }) => {
         />
         <div className="absolute top-4 left-4">
           <span
-            className={`bg-white/90 backdrop-blur-md text-gray-800 text-[10px] font-bold px-3 py-1.5 rounded-full shadow-sm ${
-              quantity > 0 ? "bg-lime-500" : "bg-red-500"
+            className={`bg-lime-400/70 backdrop-blur-md text-white text-[10px] font-bold px-3 py-1.5 rounded-full shadow-sm `}
+          >
+            {busType}
+          </span>
+        </div>
+        <div className="absolute bottom-4 right-4">
+          <span
+            className={`backdrop-blur-lg text-[10px] font-bold px-3 py-1.5 rounded-full shadow-sm ${
+              quantity > 0
+                ? "bg-white/90 text-gray-800"
+                : "bg-red-500 text-white"
             }`}
           >
             {quantity > 0 ? `${quantity} Tickets Left` : "Sold Out"}
@@ -35,10 +49,12 @@ const AllTicketPageCard = ({ ticket }) => {
           {operator}
         </h3>
 
-        <div className="flex items-center gap-2 mb-4">
+        <div className="flex items-center gap-2 mb-2">
           <span className="text-xs font-bold text-lime-600 bg-lime-50 px-2 py-1 rounded">
             {from}
           </span>
+          <div className="h-px grow bg-dashed bg-gray-300 border-t border-dashed"></div>
+          <span className="pb-1">to</span>
           <div className="h-px grow bg-dashed bg-gray-300 border-t border-dashed"></div>
           <span className="text-xs font-bold text-lime-600 bg-lime-50 px-2 py-1 rounded">
             {to}
@@ -54,18 +70,7 @@ const AllTicketPageCard = ({ ticket }) => {
           </div>
         </div>
 
-        <div className="flex flex-wrap gap-1.5 mb-6">
-          {perks?.slice(0, 3).map((perk, idx) => (
-            <span
-              key={idx}
-              className="text-[9px] font-bold bg-gray-50 text-gray-400 border border-gray-100 px-2 py-0.5 rounded uppercase"
-            >
-              • {perk}
-            </span>
-          ))}
-        </div>
-
-        <div className="mt-auto pt-4 border-t border-gray-50 flex items-center justify-between">
+        <div className="mt-auto border-t pt-4 border-gray-200 flex items-center justify-between ">
           <span className="text-2xl font-black text-gray-800 mb-2 group-hover:text-lime-600 transition-colors line-clamp-1 uppercase tracking-tight">
             ${price}
           </span>
